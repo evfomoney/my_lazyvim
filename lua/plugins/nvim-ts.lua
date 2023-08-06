2850e6a79ea4367be767bb84e42402e67a8c1c53
evfomoney <evfomoney@gmail.com>
2023-08-06 18:55:21 +0800

    添加了markdown-preview 与 nvim-treesitter

---
+++ lua/plugins/nvim-ts.lua
@@ -0,0 +1,35 @@
+return {
+   "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "bash",
+      "c",
+      "html",
+      "javascript",
+      "jsdoc",
+      "json",
+      "lua",
+      "luadoc",
+      "luap",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "query",
+      "regex",
+      "tsx",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "yaml",
+    },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<C-space>",
+        node_incremental = "<C-space>",
+        scope_incremental = false,
+        node_decremental = "<bs>",
+      },
+    },
+  }
+}
